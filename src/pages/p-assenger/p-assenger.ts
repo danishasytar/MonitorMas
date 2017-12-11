3551d150bf24b19e1bffe2a8e6a694738dd85fe2
danish <danish.asytar@megasap.com>
2017-12-11 15:06:56 +0800

    commit lagi

--- src/pages/p-assenger/p-assenger.ts
+++ src/pages/p-assenger/p-assenger.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController , AlertController} from 'ionic-angular';
 import { ProfilePage } from '../profile/profile';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'page-p-assenger',
@@ -8,10 +10,50 @@ import { ProfilePage } from '../profile/profile';
 })
 export class PASSENGERPage {
 
-  constructor(public navCtrl: NavController) {
+		pass_number: any;
+
+		passengers = [
+		{"pass_number" : 'JL00008888', "name": 'MUHAMMAD DANISH ASYTAR', "departure": "2017-12-11T14:30:51.01"},
+		{"pass_number" : 'JL00004444', "name": 'MUHAMMAD AHMAD', "departure": "2017-12-11T18:30:51.01"}
+		]
+
+  constructor(public navCtrl: NavController, private http: HttpClient, private alertCtrl: AlertController) {
+  	this.getData();
+  	console.log(this.passengers);
   }
-  goToProfile(params){
-    if (!params) params = {};
-    this.navCtrl.push(ProfilePage);
+  goToProfile(){
+  	var flag = false;
+  	for(var i=0; i<Object.keys(this.passengers).length;i++){
+  		if(this.passengers[i].pass_number == this.pass_number.toUpperCase()){
+  			console.log(this.pass_number)
+  			flag = false;
+  			this.navCtrl.push(ProfilePage, {'myParam': this.pass_number.toUpperCase()});
+  			break;
+  		}
+  		else{
+  			flag = true;
+  		}
+
+  	}
+  	if(flag){
+		  let alert = this.alertCtrl.create({
+		    title: 'No pass number found',
+		    buttons: ['OK']
+		  });
+		  alert.present();
+  	}
+    // if (!params) params = {};
+    // this.navCtrl.push(ProfilePage);
   }
+  getData(){
+       this.http.get('http://unwilled-children.000webhostapp.com/api/type_meal',{} )
+          .subscribe(data => {
+            console.log();
+                        
+          }, err => {
+            console.log(err);
+          });
+  }
+
+
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,13 +1,124 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+export interface CountdownTimer {
+  seconds: number;
+  secondsRemaining: number;
+  runTimer: boolean;
+  hasStarted: boolean;
+  hasFinished: boolean;
+  displayTime: string;
+}
+
 
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html'
 })
-export class ProfilePage {
 
-  constructor(public navCtrl: NavController) {
+export class ProfilePage {
+departuretime;
+departuretimedisplay;
+
+passengers = [
+		{"pass_number" : 'JL00008888', "name": 'MUHAMMAD DANISH ASYTAR', "departure": "2017-12-11T14:30:51.01"},
+		{"pass_number" : 'JL00004444', "name": 'MUHAMMAD AHMAD', "departure": "2017-12-11T18:30:51.01"}
+		]
+passengerDetail;
+name = "";
+  constructor(navParams: NavParams, public navCtrl: NavController) {
+  
+  	console.log(this.departuretimedisplay);
+  	let myParam = navParams.get('myParam');
+  	console.log(myParam)
+  	for(var i=0;i<Object.keys(this.passengers).length;i++){
+  		if(this.passengers[i].pass_number == myParam){
+  			this.passengerDetail = this.passengers[i];
+  		}
+  	}
+  	console.log(this.passengerDetail);
+  	this.name = this.passengerDetail.name;
+ 	this.departuretime = new Date( this.passengerDetail.departure);
+  	this.departuretimedisplay = this.departuretime.getHours() +':'+ this.departuretime.getMinutes()
+
+
   }
   
+  gettime(): number {
+    var d = new Date("2017-12-11T14:30:51.01"); 
+    var d2 = new Date(); // for now
+    console.log(d2)
+    console.log(d)
+    return (d.getTime()-d2.getTime())/1000;
+
+  }
+
+  timeInSeconds: number = this.gettime();
+  timer: CountdownTimer;
+
+
+  ngOnInit() {
+    this.initTimer();
+    this.startTimer();
+  }
+
+  hasFinished() {
+    return this.timer.hasFinished;
+  }
+
+  initTimer() {
+    if (!this.timeInSeconds) { this.timeInSeconds = 0; }
+
+    this.timer = <CountdownTimer>{
+      seconds: this.timeInSeconds,
+      runTimer: false,
+      hasStarted: false,
+      hasFinished: false,
+      secondsRemaining: this.timeInSeconds
+    };
+
+    this.timer.displayTime = this.getSecondsAsDigitalClock(this.timer.secondsRemaining);
+  }
+
+  startTimer() {
+    this.timer.hasStarted = true;
+    this.timer.runTimer = true;
+    this.timerTick();
+  }
+
+  pauseTimer() {
+    this.timer.runTimer = false;
+  }
+
+  resumeTimer() {
+    this.startTimer();
+  }
+
+  timerTick() {
+    setTimeout(() => {
+      if (!this.timer.runTimer) { return; }
+      this.timer.secondsRemaining--;
+      this.timer.displayTime = this.getSecondsAsDigitalClock(this.timer.secondsRemaining);
+      if (this.timer.secondsRemaining > 0) {
+        this.timerTick();
+      } else {
+        this.timer.hasFinished = true;
+      }
+    }, 1000);
+  }
+
+  getSecondsAsDigitalClock(inputSeconds: number) {
+    const secNum = parseInt(inputSeconds.toString(), 10); // don't forget the second param
+    const hours = Math.floor(secNum / 3600);
+    const minutes = Math.floor((secNum - (hours * 3600)) / 60);
+    const seconds = secNum - (hours * 3600) - (minutes * 60);
+    let hoursString = '';
+    let minutesString = '';
+    let secondsString = '';
+    hoursString = (hours < 10) ? '0' + hours : hours.toString();
+    minutesString = (minutes < 10) ? '0' + minutes : minutes.toString();
+    secondsString = (seconds < 10) ? '0' + seconds : seconds.toString();
+    return hoursString + ':' + minutesString + ':' + secondsString;
+  }
 }
